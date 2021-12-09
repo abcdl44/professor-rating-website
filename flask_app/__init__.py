cf216f757d6c0d57ed8cb5bcf976d8afa49691d5
Wesley Chen <wesleychen44@gmail.com>
2021-12-09 16:06:22 -0500

    It runs
    Yeah it runs now it's just ugly as shit.

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -30,6 +30,9 @@ def create_app(test_config=None):
     login_manager.init_app(app)
     bcrypt.init_app(app)
 
+    app.config["MONGODB_HOST"] = "mongodb://localhost:27017/not_sure"
+    app.config["SECRET_KEY"] = os.urandom(16)
+
     app.register_error_handler(404, page_not_found)
     app.register_blueprint(users)
     app.register_blueprint(not_users)
