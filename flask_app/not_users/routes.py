a2887e902f17bc420e155b7b0afc6c32b092f11a
Wesley Chen <wesleychen44@gmail.com>
2021-12-09 22:17:16 -0500

    Fixed search
    ^

--- flask_app/not_users/routes.py
+++ flask_app/not_users/routes.py
@@ -21,11 +21,7 @@ def index():
 
 @not_users.route("/search_results/<search>", methods=["GET"])
 def search_results(search):
-    results = Professor.objects.search_text(search)
-    # results = None
-    # if results is None:
-        # return render_template("404.html")
-
+    results = Professor.objects(name__contains=search)
     return render_template("search_results.html", results = results)
 
 
